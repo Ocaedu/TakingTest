aa326c8444f8fa6aaaac749f625be1ac3eaa8e62
Carlos <carlos.silva@7comm.com.br>
2024-12-04 01:19:49 -0300

    feat criação do banco e config de entidades para inserção

--- TakingTest/TakingTest/Program.cs
+++ TakingTest/TakingTest/Program.cs
@@ -1,9 +1,13 @@
-using Microsoft.EntityFrameworkCore;
+
 using TakingTest.Domain.Interfaces.Services;
 using TakingTest.Domain.Services;
 using TakingTest.Domain.Interfaces.Repositories;
 using TakingTest.Infra.Contexts;
 using TakingTest.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using TakingTest.Application.Interfaces;
+using TakingTest.Application.Services;
+using TakingTest.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,14 +19,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //Adicionando o contexto e confiurando a string de conexão
 var connectionString = builder.Configuration.GetConnectionString("Default");
+
 builder.Services.AddDbContext<SaleContext>(x => x.UseSqlite(connectionString));
 //Inserindo os serviços no container
 
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 builder.Services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
 builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+builder.Services.AddScoped<IBranchRepository, BranchRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddScoped<ISaleApp, SaleAppService>();
+builder.Services.AddScoped<IBranchService, BranchService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
+builder.Services.AddAutoMapper(x => x.AddProfile(new MappingEntity()));
 
 var app = builder.Build();
 
